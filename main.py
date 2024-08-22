34e8faa0ff19ee82692f180dfbe45d3b87a23729
Juliane Bueno <julianebueno1@gmail.com>
2024-08-21 22:37:09 -0300

    correção na criação de dados e mensagens de erro

--- main.py
+++ main.py
@@ -6,17 +6,19 @@ import sqlite3
 app = FastAPI()
 con = sqlite3.connect("itemsDatabase.db")
 cur = con.cursor()
-
-cur.execute("DROP TABLE IF EXISTS items")
 cur.execute("CREATE TABLE IF NOT EXISTS items(name, price, is_offer)")
-cur.execute("""
-    INSERT INTO items VALUES
-        ('Bola', 3, true),
-        ('Dado', 5, false),
-        ('Cubo', 10, true),
-        ('Piramide', 7, false)
-""")
-con.commit()
+cur.execute("SELECT * FROM items")
+items = cur.fetchall()
+if items == None:
+    cur.execute("""
+        INSERT INTO items VALUES
+            ('Bola', 3, true),
+            ('Dado', 5, false),
+            ('Cubo', 10, true),
+            ('Piramide', 7, false)
+    """)
+    con.commit()
+
 
 class Item(BaseModel):
     name: str
@@ -31,18 +33,24 @@ async def read_root():
 async def read_items():
     cur.execute("SELECT * FROM items")
     items = cur.fetchall()
+    if items == None:
+        return {"message": "Nenhum item encontrado"}
     return items
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     cur.execute("SELECT * FROM items WHERE rowid = ?", (item_id,))
     item = cur.fetchone()
+    if item == None:
+        return {"message": "Item não encontrado"}
     return item
 
-@app.post("/items/")
+@app.post("/items")
 async def create_item(item: Item):
     cur.execute("INSERT INTO items VALUES (?, ?, ?)", (item.name, item.price, item.is_offer))
     con.commit()
+    if cur.rowcount == 0:
+        return {"message": "Erro ao inserir item"}
     return item
 
 @app.put("/items/{item_id}")
@@ -55,4 +63,6 @@ async def update_item(item_id: int, item: Item):
 async def delete_item(item_id: int):
     cur.execute("DELETE FROM items WHERE rowid = ?", (item_id,))
     con.commit()
+    if cur.rowcount == 0:
+        return {"message": "Item não encontrado"}
     return {"message": "Item deletado"}

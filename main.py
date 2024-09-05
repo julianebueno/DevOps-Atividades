4df47550d15d9f19765ba4593b8da66d7e68212d
Juliane Bueno <julianebueno1@gmail.com>
2024-09-04 21:06:24 -0300

    correção comentários funções

--- main.py
+++ main.py
@@ -21,62 +21,55 @@ if itemsDB is None:
     """)
     con.commit()
 
-""" Classe Item """
-
 class Item(BaseModel):
+    """ Classe Item """
     name: str
     price: float
     is_offer: Union[bool, None] = None
 
-""" Rotas """
-
 @app.get("/")
 async def read_root():
+    """ Rotas """
     return {"message": "Api de controle de itens"}
-
-""" GET TODOS OS ITEM """
 
 @app.get("/items")
 async def read_items():
+    """ GET TODOS OS ITEM """
     cur.execute("SELECT * FROM items")
     items = cur.fetchall()
     if items is None:
         return {"message": "Nenhum item encontrado"}
     return items
 
-""" GET ITEM POR ID """
-
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
+    """ GET ITEM POR ID """
     cur.execute("SELECT * FROM items WHERE rowid = ?", (item_id,))
     item = cur.fetchone()
     if item is None:
         return {"message": "Item não encontrado"}
     return item
 
-""" POST ITEM """
-
 @app.post("/items")
 async def create_item(item: Item):
+    """ POST ITEM """
     cur.execute("INSERT INTO items VALUES (?, ?, ?)", (item.name, item.price, item.is_offer))
     con.commit()
     if cur.rowcount == 0:
         return {"message": "Erro ao inserir item"}
     return item
 
-""" PUT ITEM POR ID """
-
 @app.put("/items/{item_id}")
 async def update_item(item_id: int, item: Item):
+    """ PUT ITEM POR ID """
     cur.execute("UPDATE items SET name = ?, price = ?, is_offer = ? WHERE rowid = ?" ,
                 (item.name, item.price, item.is_offer, item_id))
     con.commit()
     return item
 
-""" DELETE ITEM POR ID """
-
 @app.delete("/items/{item_id}")
 async def delete_item(item_id: int):
+    """ DELETE ITEM POR ID """
     cur.execute("DELETE FROM items WHERE rowid = ?", (item_id,))
     con.commit()
     if cur.rowcount == 0:

465ce75dc8609ccc813a69feab1e9d1355d8c6c3
Juliane Bueno <julianebueno1@gmail.com>
2024-09-30 19:07:58 -0300

    correções build

--- test/tests.py
+++ test/tests.py
@@ -1,38 +1,47 @@
+""" Testes """
+
 from src.main import *
 import pytest
 import pytest_asyncio
 
 @pytest.mark.asyncio
 async def test_root():
-  result = await read_root()
-  assert result == {"message": "Api de controle de itens"}
+    """ Teste Raiz """
+    result = await read_root()
+    assert result == {"message": "Api de controle de itens"}
 
 @pytest.mark.asyncio
 async def test_read_items():
-  result = await read_items()
-  assert not result == {"message": "Nenhum item encontrado"}
+    """ Teste GET TODOS OS ITEM """
+    result = await read_items()
+    assert not result == {"message": "Nenhum item encontrado"}
 
 @pytest.mark.asyncio
 async def test_read_item():
-  test_item_id = 1
-  result = await read_item(test_item_id)
-  assert not result == {"message": "Item não encontrado"}
+    """ Teste GET ITEM POR ID """
+    test_item_id = 1
+    result = await read_item(test_item_id)
+    assert not result == {"message": "Item não encontrado"}
 
 @pytest.mark.asyncio
 async def test_create_item():
-  test_item = Item(name="Teste", price=10, is_offer=True)
-  result = await create_item(test_item)
-  assert result == test_item
+    """ Teste POST ITEM """
+    test_item = Item(name="Teste", price=10, is_offer=True)
+    result = await create_item(test_item)
+    assert result == test_item
 
 @pytest.mark.asyncio
 async def test_update_item():
-  test_item_id = 1
-  test_item = Item(name="Teste", price=10, is_offer=True)
-  result = await update_item(test_item_id, test_item)
-  assert result == test_item
+    """ Teste PUT ITEM POR ID """
+    test_item_id = 1
+    test_item = Item(name="Teste", price=10, is_offer=True)
+    result = await update_item(test_item_id, test_item)
+    assert result == test_item
 
 @pytest.mark.asyncio
 async def test_delete_item():
-  test_item_id = 1
-  result = await delete_item(test_item_id)
-  assert result == {"message": "Item deletado"}
+    """ Teste DELETE ITEM POR ID """
+    test_item_id = 1
+    result = await delete_item(test_item_id)
+    assert result == {"message": "Item deletado"}
+    
